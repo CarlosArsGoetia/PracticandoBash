11a26c214eca3bf56ba8a5a236896849b8694c17
CarHerJR <74396964+CarlosArsGoetia@users.noreply.github.com>
2025-04-12 10:03:35 -0300

    Update obtenhtml.sh

--- obtenhtml.sh
+++ obtenhtml.sh
@@ -1,9 +1,22 @@
-#!/bin/bash
+#!/bin/bash 
 
-url=$1
 
-CloneFile="pagina_web_$(date +%Y-%m-%d_%H).html"
+function ctrl_c(){
+ echo -e "\n\n[!] Saliendo ...\n"
+tput cnorm; exit 1
+}
 
-curl -s "$url" > "$CloneFile"
+trap  ctrl_c SIGINT
 
-echo "El código fuente de la página web $url se ha guardado en $CloneFile"
+
+web=$1
+
+if [[ -z $web ]]; then
+  echo "Error: Debes proporcionar el nombre de la Web para clonar."
+  echo "Ejemplo: "
+  exit 1
+fi
+
+echo "Run script $0  para clonar la web :D "
+echo "Clonando $web en el directorio $web..."
+wget --mirror --page-requisites --adjust-extension --convert-links --recursive -P "$web" $web 
